e4eb0263f58fc2b044b2285a99cd9166276584c0
sijokgeorge <sijo.george@marlabs.com>
2015-10-09 12:11:41 +0530

    root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
+
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   namespace :api do
     api version: 1, module: 'v1' do
       resources :sessions, only: [:create]
