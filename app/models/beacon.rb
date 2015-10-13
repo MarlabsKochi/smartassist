48f4ab3b5c9de0aaf4bc64936923070aa2d04db7
Manoj <mhmenon910@gmail.com>
2015-10-13 20:12:42 +0530

    validation for message in beacon crud

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -1,3 +1,3 @@
 class Beacon < ActiveRecord::Base
-  validates :uuid, :major_number, :minor_number, :service_point_name, presence: true
+  validates :uuid, :major_number, :minor_number, :service_point_name, :message, presence: true
 end
