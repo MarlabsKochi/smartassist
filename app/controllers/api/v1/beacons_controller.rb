2548fa20f94682945580925d8bce6d0035eb734d
Manoj <mhmenon910@gmail.com>
2015-10-13 14:52:43 +0530

    minor change

--- app/controllers/api/v1/beacons_controller.rb
+++ app/controllers/api/v1/beacons_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::BeaconsController < Api::V1::BaseController
-	def beacon_details
-      beacon_details = Beacon.find_by_uuid(params["beacon"]["uuid"])
+  def beacon_details
+    beacon_details = Beacon.find_by_uuid(params["beacon"]["uuid"])
       if beacon_details 
-      	      expose({
-      	      	context: beacon_details.service_point_name
-      })
+        expose({
+      	  context: beacon_details.service_point_name
+        })
       else
       	error! :not_found
       end
-	end
+   end
 end
