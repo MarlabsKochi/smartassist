ba2db6ba96b4be10b5049b6b925d356c9bf738b8
Manoj <mhmenon910@gmail.com>
2015-10-13 14:37:00 +0530

    expose beacon details

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
   namespace :api do
     api version: 1, module: 'v1' do
       resources :sessions, only: [:create]
+      resources :beacons, only: [] do
+        get 'beacon_details', on: :member
+      end
     end
   end
 
