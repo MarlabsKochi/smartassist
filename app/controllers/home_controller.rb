192e9dda308cbb60649eb2320468d041d80b56e3
sijokg <cijo_k_george@yahoo.co.in>
2015-10-19 11:53:33 +0530

    indentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   end
 
   def patient_near_by
-  	@patients = Appointment.where(:user_status_id => 1).pluck(:user_status_id)
+    @patients = Appointment.where(:user_status_id => 1).pluck(:user_status_id)
   end
 end
